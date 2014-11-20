ae0eaa9fcaaf8120fa530f5c360ba9c98cd4ff83
user <user@localhost>
2014-11-20 01:52:12 -0500

    Created base code for most controllers and skeleton models

---
+++ eFood1/src/model/Login.java
@@ -0,0 +1,13 @@
+package model;
+
+import javax.security.auth.login.LoginException;
+
+public class Login {
+
+	public String getSecurityToken(String username, String password) throws LoginException {
+		// TODO Auto-generated method stub
+		return null;
+		
+	}
+
+}
