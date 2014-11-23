8616048dadc4cfd71e40e060a5f9d4757224aacb
ethan <user@localhost>
2014-11-23 18:59:46 -0500

    update jsps

--- eFood1/src/ctrl/Front.java
+++ eFood1/src/ctrl/Front.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Front
  */
-@WebServlet("/*")
+@WebServlet("/1")
 public class Front extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
