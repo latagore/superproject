656e4923301133e1309f4351fd99bab2435bcf94
ethan <user@localhost>
2014-11-24 00:26:43 -0500

    update link to jsp

--- eFood1/src/ctrl/Front.java
+++ eFood1/src/ctrl/Front.java
@@ -18,7 +18,7 @@ import model.Login;
 /**
  * Servlet implementation class Front
  */
-@WebServlet("/*")
+@WebServlet("/Front/*")
 public class Front extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
--- eFood1/src/ctrl/HomePageController.java
+++ eFood1/src/ctrl/HomePageController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -32,13 +33,14 @@ public class HomePageController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
 			// get all the categories and serve the jsp
+			String jsp = "/index.jspx";
 			Category c = (Category) this.getServletContext().getAttribute("category");
 			List<String> l;
 			l = c.getCategories();
 
 			request.setAttribute("categories", l);
-			request.getRequestDispatcher("/WEB-INF/HomePage.jspx")
-			.forward(request, response);
+			RequestDispatcher rd = request.getRequestDispatcher(jsp);
+			rd.forward(request,response);
 		} catch (SQLException e) {
 			// TODO Need a message?
 			throw new ServletException(e);
